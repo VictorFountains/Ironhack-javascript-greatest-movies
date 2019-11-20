961d53c25a5ccd7b32ce2e6243d6fac37e32f2cb
Victor Fuentes <21144@columbushs.com>
2019-11-20 13:21:45 -0500

    first commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,7 +2,44 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+
+function orderByYear(array){
+
+    let copy = [...array];
+
+    copy.sort( (a, b) =>{
+        if(a.year < b.year){
+            return -1;
+        }
+        else if(b.year < a.year){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    })
+
+    return copy;
+}
+
+
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+
+function howManyMovies(arrMovie){
+    let arrDrama = arrMovie.filter((movie) => {
+      if (movie.genre.includes("Drama")){
+        return true;
+      }
+      else{
+        return false;
+      }
+    });
+    let howMany = arrDrama.reduce((a,b) =>{
+      return accumlator + Number(value.director === "Steven Spielberg");
+     },0 );
+     return howMany
+ }
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
