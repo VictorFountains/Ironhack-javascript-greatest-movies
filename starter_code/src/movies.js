a3bc178f7d3ec62f5a77a74667b344ebb203cdb6
Victor Fuentes <21144@columbushs.com>
2019-11-22 10:24:37 -0500

    home

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,47 +3,56 @@
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
 
-function orderByYear(array){
+function orderByYear(array) {
 
-    let copy = [...array];
+  let copy = [...array];
 
-    copy.sort( (a, b) =>{
-        if(a.year < b.year){
-            return -1;
-        }
-        else if(b.year < a.year){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    })
+  copy.sort((a, b) => {
+    if (a.year < b.year) {
+      return -1;
+    }
+    else if (b.year < a.year) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  })
 
-    return copy;
+  return copy;
 }
 
 
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
-function howManyMovies(arrMovie){
-    let arrDrama = arrMovie.filter((movie) => {
-      if (movie.genre.includes("Drama")){
-        return true;
-      }
-      else{
-        return false;
-      }
-    });
-    let howMany = arrDrama.reduce((a,b) =>{
-      return a + Number(b.director === "Steven Spielberg");
-     },0 );
-     return howMany
- }
+function howManyMovies(arrMovie) {
+  let arrDrama = arrMovie.filter((movie) => {
+    if (movie.genre.includes("Drama")) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  });
+  let howMany = arrDrama.reduce((a, b) => {
+    return a + Number(b.director === "Steven Spielberg");
+  }, 0);
+  return howMany
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
-
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+
+function ratesAverage(arr) {
+  let arr = movies.reduce( (a,c) =>{
+    return a + c.rate;
+  }, 0);
+  let avgRate = arr / movie.rate.length;
+  return avgRate
+};
+
+
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
